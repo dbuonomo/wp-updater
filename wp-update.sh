87b1e77ea1a1e0c9bdf744e4f5e4e087eeed0238
David Buonomo <dnb@blueatlas.com>
2018-07-06 10:19:11 -0400

    Initial commit.

---
+++ wp-update.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+file="/etc/apache2/sites-available/$1.conf"
+if [ -f "$file" ]
+then
+    echo "$file found."
+else
+    echo "$file not found."
+    echo "Update aborted."
+    exit 1
+fi
+
+echo Downloading Wordpress...
+wget -q https://wordpress.org/latest.zip
+mkdir -p download
+unzip -qq latest.zip -d download
+
+cver="`grep '^\$wp_version' wp-includes/version.php | grep -o '[0-9]\.*[0-9]\.*[0-9]'`"
+nver="`grep '^\$wp_version' download/wordpress/wp-includes/version.php | grep -o '[0-9]\.*[0-9]\.*[0-9]'`"
+
+read -p "Wordpress will be updated from v$cver to v$nver. Do you wish to continue (y/n)? " input
+case $input in
+    [Yy]* ) make install; break;;
+    [Nn]* ) exit;;
+    * ) echo "Invalid input, aborting.";;
+esac
+
+echo Update starts in 5 seconds, type Ctrl+c to abort...
+sleep 5
+
+echo Disabling $1...
+sleep 2
+a2dissite $1.conf
+service apache2 reload
+
+rm -rf wp-admin wp-includes
+cp -rf download/wordpress/* .
+
+echo Enabling $1...
+sleep 2
+a2ensite $1.conf
+service apache2 reload
+
+echo 'Cleanup...'
+rm -rf latest.zip download/wordpress
+
+echo Update complete.
+exit 0
